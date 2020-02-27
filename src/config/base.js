5385a1ba6e7e9804145f52fc4ba2cbc7a0fa153d
Acllex <986169823@qq.com>
2020-02-27 23:12:08 +0800

    'base'

--- src/config/base.js
+++ src/config/base.js
@@ -1,5 +1,5 @@
 const SUCCESS = 200;
 const FAIL = 404;
-const BURL = "http://localhost/shopserver/public/"
+const BURL = "http://39.105.220.73/shopserver/public"
 //const BASEURL = "http//"
 export {SUCCESS, FAIL, BURL};
